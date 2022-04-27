a43040905d4def616a1aaa8fc1c598ec89cef960
MaximGromak <maximgromak@gmail.com>
2022-04-27 23:32:36 +1000

    add: card layout

--- src/components/sections/map/Map.js
+++ src/components/sections/map/Map.js
@@ -1,5 +1,10 @@
 import React from "react";
 
+import logo from '../../../pics/map/logo.jpg';
+import vk from '../../../pics/social/vk.png';
+import tg from '../../../pics/social/telegram.png';
+import wa from '../../../pics/social/whatsapp.png';
+
 export default function Map(props) {
     return (
         <div className='wrapper-map flex-col'>
@@ -8,15 +13,27 @@ export default function Map(props) {
                 <iframe
                     src="https://yandex.ru/map-widget/v1/?um=constructor%3Abd7007c3f41c78d9a2d2bf8da5420b2bc4faa7bacfe5aea6e09acb80345eea08&amp;source=constructor"
                     width="100%" height="540" frameBorder="0"/>
-                <div className='map-card pos-abs b1'>
-                    <div className='card-big-logo'>
-
+                <div className='map-card pos-abs'>
+                    <div className='card-logo-wrapper'>
+                        <img className='card-logo' src={logo}/>
                     </div>
-                    <div className='card-sublogo'></div>
+                    <div className='card-title'>
+                        <span>nebrevno</span>
+                    </div>
+                    <div className='card-subtitle'>
+                        <span>stretch studio</span>
+                    </div>
                     <div className='card-address'>
+                        <span>ул. Пионерская 1, этаж 3, офис 312</span>
                     </div>
-                    <div className='card-phones'></div>
-                    <div className='social-links'></div>
+                    <div className='card-phones'>
+                        <span>8 (914) 209-72-94</span>
+                    </div>
+                    <div className='social-links flex-row'>
+                        <img className='social-link vk' src={vk}/>
+                        <img className='social-link wa' src={wa}/>
+                        <img className='social-link tg' src={tg}/>
+                    </div>
                 </div>
             </div>
         </div>
